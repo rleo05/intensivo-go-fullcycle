5ecc491444e30554822ea0ab8be55143c8c22f65
Leonardo <leorod2005@gmail.com>
2024-08-24 17:22:10 -0300

    dia 1

---
+++ internal/web/handler.go
@@ -0,0 +1,103 @@
+package web
+
+import (
+	"encoding/json"
+	"gobooks/internal/service"
+	"net/http"
+	"strconv"
+)
+
+type BookHandlers struct {
+	service *service.BookService
+}
+
+func NewBookHandlers(s *service.BookService) *BookHandlers {
+	return &BookHandlers{service: s}
+}
+
+func (h *BookHandlers) GetBooks(w http.ResponseWriter, r *http.Request) {
+	books, err := h.service.GetBooks()
+	if err != nil {
+		http.Error(w, "failed to get Books", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func (h *BookHandlers) CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book service.Book
+	err := json.NewDecoder(r.Body).Decode(&book)
+	if err != nil {
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
+		return 
+	}
+
+	err = h.service.CreateBook(&book)
+	if err != nil {
+		http.Error(w, "failed to create book", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(book)
+}
+
+func (h *BookHandlers) GetBookByID(w http.ResponseWriter, r *http.Request){
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	book, err := h.service.GetBookByID(id)
+	if err != nil {
+		http.Error(w, "failed to get book", http.StatusInternalServerError)
+	}
+	if book == nil {
+		http.Error(w, "book not found", http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(book)
+}
+
+func (h *BookHandlers) UpdateBook(w http.ResponseWriter, r *http.Request){
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid book ID", http.StatusBadRequest)
+		return
+	}
+
+	var book service.Book
+	if err := json.NewDecoder(r.Body).Decode(&book); err != nil {
+		http.Error(w, "invalid request payload", http.StatusBadRequest)
+		return
+	}
+	book.ID = id
+
+	if err := h.service.UpdateBook(&book); err != nil{
+		http.Error(w, "failed to update book", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(book)
+}
+
+func (h *BookHandlers) DeleteBook(w http.ResponseWriter, r *http.Request){
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "invalid book ID", http.StatusBadRequest)
+	}
+
+	if err := h.service.DeleteBook(id); err != nil {
+		http.Error(w, "failed to delete book", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
